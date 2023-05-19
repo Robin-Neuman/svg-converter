c0bd8df19e981685cf8156f332f088f17bbdcac0
Robin Neuman <robin.neuman.dev@gmail.com>
2023-05-19 18:24:58 +0200

    added funcationality for removing clippath

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ var fs = require('fs');
 
 async function convert(path) {
 	let files = await fs.promises.opendir(path);
+
+	const regex1 = new RegExp(`xmlns:xlink`, 'ig');
+	const regex2 = new RegExp(`xml:space`, 'ig');
+	const regex3 = new RegExp(`xmlns:serif`, 'ig');
+	const regex4 = new RegExp('serif:id', 'ig');
+	const regex5 = /clip-path="url\(#_clip1\)"/;
+
 	for await (const file of files) {
 		const isDirectory = (await fs.promises.lstat(`${path}/${file.name}`)).isDirectory();
 		if (!isDirectory) {
 			fs.readFile(`${path}/${file.name}`, 'utf-8', function (err, data) {
 				if (err) throw err;
-
-				const regex1 = new RegExp(`xmlns:xlink`, 'ig');
-				const regex2 = new RegExp(`xml:space`, 'ig');
-				const regex3 = new RegExp(`xmlns:serif`, 'ig');
-				const regex4 = new RegExp('serif:id', 'ig');
 
 				let svg = data;
 
@@ -19,6 +21,7 @@ async function convert(path) {
 				svg = svg.replace(regex2, `xmlspace`);
 				svg = svg.replace(regex3, `xmlnsserif`);
 				svg = svg.replace(regex4, 'serifid');
+				svg = svg.replace(regex5, '');
 
 				fs.writeFile(`${path}/${file.name}`, svg, 'utf-8', function (err, data) {
 					if (err) throw err;
@@ -33,17 +36,13 @@ async function convert(path) {
 					fs.readFile(`${path}/${file.name}/${fileDeep.name}`, 'utf-8', function (err, data) {
 						if (err) throw err;
 
-						const regex1 = new RegExp(`xmlns:xlink`, 'ig');
-						const regex2 = new RegExp(`xml:space`, 'ig');
-						const regex3 = new RegExp(`xmlns:serif`, 'ig');
-						const regex4 = new RegExp('serif:id', 'ig');
-
 						let svg = data;
 
 						svg = svg.replace(regex1, `xmlnsxlink`);
 						svg = svg.replace(regex2, `xmlspace`);
 						svg = svg.replace(regex3, `xmlnsserif`);
 						svg = svg.replace(regex4, 'serifid');
+						svg = svg.replace(regex5, '');
 						data.replace;
 
 						fs.writeFile(`${path}/${file.name}/${fileDeep.name}`, svg, 'utf-8', function (err, data) {
@@ -64,17 +63,13 @@ async function convert(path) {
 								function (err, data) {
 									if (err) throw err;
 
-									const regex1 = new RegExp(`xmlns:xlink`, 'ig');
-									const regex2 = new RegExp(`xml:space`, 'ig');
-									const regex3 = new RegExp(`xmlns:serif`, 'ig');
-									const regex4 = new RegExp(`serif:id`, 'ig');
-
 									let svg = data;
 
 									svg = svg.replace(regex1, `xmlnsxlink`);
 									svg = svg.replace(regex2, `xmlspace`);
 									svg = svg.replace(regex3, `xmlnsserif`);
 									svg = svg.replace(regex4, 'serifid');
+									svg = svg.replace(regex5, '');
 
 									fs.writeFile(
 										`${path}/${file.name}/${fileDeep.name}/${fileDeeper.name}`,
